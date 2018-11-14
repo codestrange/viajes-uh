72503433a25b7c49380a35eb3728c8fa78bc869f
leynier <leynier41@gmail.com>
2018-11-14 10:12:48 -0500

    Implementar estructura del backend

---
+++ backend/run.py
@@ -0,0 +1,45 @@
+from os import getenv
+from os.path import abspath, dirname, join
+from coverage import coverage
+from flask_script import Manager, Shell
+
+# Coverage debe empezar antes de las importaciones
+# de los módulos de las aplicación
+COV = coverage(branch=True, include='app/*')
+COV.start()
+
+from app import create_app
+from app import db
+
+app = create_app(getenv('FLASK_CONFIG') or 'default')
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    from unittest import TestLoader, TextTestRunner
+    tests = TestLoader().discover('tests')
+    TextTestRunner(verbosity=2).run(tests)
+    COV.stop()
+    COV.save()
+    print('\nCoverage Summary:')
+    COV.report()
+    basedir = abspath(dirname(__file__))
+    covdir = join(basedir, 'tmp/coverage')
+    COV.html_report(directory=covdir)
+    print(f'HTML version: file://{covdir}/index.html')
+    COV.erase()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db)
+
+
+# Shell de flask_script con IPython
+manager = Manager(app)
+manager.add_command(Shell(make_context=make_shell_context))
+
+
+if __name__ == '__main__':
+    manager.run()
--- frontend/README.md
+++ frontend/README.md
@@ -1,16 +1,19 @@
 # frontend
 
 ## Project setup
-```
+
+```bash
 npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+```bash
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```bash
 npm run build
 ```
