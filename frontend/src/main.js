46098101a59115474daa80989e49a68e79dda74c
leynier <leynier41@gmail.com>
2018-11-15 19:59:42 -0500

    Modificar la identación y el nombre de la aplicación

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
