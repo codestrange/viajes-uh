d5a13ca9e0fc6200f3b605233ca00ae2ef8efc69
leynier <leynier41@gmail.com>
2018-11-16 20:53:01 -0500

    Implementar los metodos get del controlador User

--- backend/app/controllers/user.py
+++ backend/app/controllers/user.py
@@ -1,14 +1,20 @@
+from flask import jsonify, abort
 from . import api
+from ..models.user import User
 
 
 @api.route('/users/', methods=['GET'])
 def get_users():
-    pass
+    users = User.query().all()
+    return jsonify([user.to_json() for user in users])
 
 
 @api.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    pass
+    user = User.query().get(id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_json())
 
 
 @api.route('/users/', methods=['POST'])
