769d765b86eb8085644bb06208113bb75b2d6a53
cbermudez97 <cbermu.97@gmail.com>
2018-11-20 04:30:34 -0500

    Implementar la funcionalidad de logueo
    Se hicieron los cambios necesarios para reflejar el logueo del u-
    suario.

--- frontend/src/components/Auth.vue
+++ frontend/src/components/Auth.vue
@@ -1,34 +1,45 @@
 <template>
     <div id="auth">
         <b-input-group @keyup.enter="validateUser()" size="sm">
-            <b-form-input placeholder="Usuario" v-model="username" ></b-form-input>
+            <b-form-input placeholder="Usuario" v-model.trim="username" :state="!wrong_input">
+            </b-form-input>
         </b-input-group>
         <b-input-group @keyup.enter="validateUser()" size="sm">
-            <b-form-input placeholder="Contraseña" type="password" v-model="password" ></b-form-input>
+            <b-form-input placeholder="Contraseña" type="password" v-model="password" :state="!wrong_input">
+            </b-form-input>
         </b-input-group>
-    </div>   
+    </div>
 </template>
 
 <script>
-export default {
-    name:'Auth',
-    data() {
-        return {
-            username:'',
-            password:'',
-        }
-    },
-    methods: {
-        validateUser() {
-            console.log('Username:' + this.username + ' Password:' + this.password);
-            //Now we should connect to the server and try to get the validation
-            //For now we would use a mock response
-            this.$store.state.user.token = this.getToken(this.username, this.password);
-        },
-        getToken(username, password) {
-            return 'mock_token';
-        },
+    import UserController from '@/controllers/user';
 
-    },
-}
+    export default {
+        name: 'Auth',
+        data() {
+            return {
+                username: '',
+                password: '',
+                wrong_input: false,
+            }
+        },
+        methods: {
+            validateUser() {
+                this.getToken(this.username, this.password);
+            },
+            getToken(username, password) {
+                UserController.getToken(username, password)
+                    .then(json => {
+                        if (json.token != null) {
+                            this.$store.state.user.token = json.token;
+                        }
+                        else {
+                            console.log(json.error + ':' + json.message);
+                            this.wrong_input = true;
+                            setTimeout(() => this.wrong_input = false, 1000);
+                        }
+                    });
+            }
+        },
+    }
 </script>
