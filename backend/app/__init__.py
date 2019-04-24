d212e412e0e8f5af6021b3cc9783f2fc056704f3
leynier <leynier41@gmail.com>
2019-04-23 23:14:23 -0400

    Reimplementar la base de datos

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from .admin import admin, ModelView, UserModelView
 from .config import config
-from .database import db, Permission, Role, User
+from .database import db, Area, Country, Document, Region, Role, Travel, TypeDocument, User, \
+    WorkflowState
 
 migrate = Migrate()
 
@@ -18,9 +19,15 @@ def create_app(config_name):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    admin.add_view(ModelView(Permission, db.session))
+    admin.add_view(ModelView(Area, db.session))
+    admin.add_view(ModelView(Country, db.session))
+    admin.add_view(ModelView(Document, db.session))
+    admin.add_view(ModelView(Region, db.session))
     admin.add_view(ModelView(Role, db.session))
+    admin.add_view(ModelView(Travel, db.session))
+    admin.add_view(ModelView(TypeDocument, db.session))
     admin.add_view(UserModelView(User, db.session))
+    admin.add_view(ModelView(WorkflowState, db.session))
 
     from .controllers import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
-db = SQLAlchemy()
+db = SQLAlchemy(session_options={"autoflush": False})
 
 user_role = db.Table('user_role',
                      db.Column('user_id',
@@ -10,17 +10,67 @@ user_role = db.Table('user_role',
                      db.Column('role_id',
                                db.Integer, db.ForeignKey('role.id'), primary_key=True))
 
-role_permission = db.Table('role_permission',
-                           db.Column('role_id',
-                                     db.Integer, db.ForeignKey('role.id'), primary_key=True),
-                           db.Column('permission_id',
-                                     db.Integer, db.ForeignKey('permission.id'), primary_key=True))
+workflowstate_type_document = db.Table('workflowstate_type_document',
+                                       db.Column('workflowstate_id',
+                                                 db.Integer,
+                                                 db.ForeignKey('workflow_state.id'),
+                                                 primary_key=True),
+                                       db.Column('type_document_id',
+                                                 db.Integer,
+                                                 db.ForeignKey('type_document.id'),
+                                                 primary_key=True))
 
 
-class Permission(db.Model):
+class Area(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
-    roles = db.relationship('Role', secondary=role_permission, backref='permissions')
+    users = db.relationship('User', backref='area')
+    workflow_states = db.relationship('WorkflowState', backref='area')
+    ancestor_id = db.Column(db.Integer, db.ForeignKey('area.id'))
+
+    @property
+    def ancestor(self):
+        return Area.query.get(self.ancestor_id)
+
+    @ancestor.setter
+    def ancestor(self, ancestor):
+        self.ancestor_id = ancestor.id
+    
+    @property
+    def descendants(self):
+        return Area.query.filter(Area.ancestor_id==self.id).all()
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    workflow_state_id = db.Column(db.Integer, db.ForeignKey('workflow_state.id'))
+    travels = db.relationship('Travel', backref='country')
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    path = db.Column(db.String(256), unique=True, nullable=False)
+    confirmed = db.Column(db.Boolean, default=False, index=True)
+    type_id = db.Column(db.Integer, db.ForeignKey('type_document.id'))
+    travel_id = db.Column(db.Integer, db.ForeignKey('travel.id'))
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+class Region(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    countries = db.relationship('Country', backref='region')
 
     def __repr__(self):
         return f'{self.name}'
@@ -29,7 +79,30 @@ class Permission(db.Model):
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
+    default = db.Column(db.Boolean, default=False, index=True)
     users = db.relationship('User', secondary=user_role, backref='roles')
+    workflow_states = db.relationship('WorkflowState', backref='role')
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+class Travel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    workflow_state_id = db.Column(db.Integer, db.ForeignKey('workflow_state.id'))
+    documents = db.relationship('Document', backref='travel')
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+class TypeDocument(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    documents = db.relationship('Document', backref='type')
 
     def __repr__(self):
         return f'{self.name}'
@@ -42,6 +115,16 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     confirmed = db.Column(db.Boolean, default=False)
     activated = db.Column(db.Boolean, default=True)
+    area_id = db.Column(db.Integer, db.ForeignKey('area.id'))
+    travels = db.relationship('Travel', backref='user')
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        roles = Role.query.filter_by(default=True).all()
+        for role in roles:
+            role.users.append(self)
+            db.session.add(role)
+        db.session.commit()
 
     @property
     def password(self):
@@ -56,3 +139,31 @@ class User(db.Model):
 
     def __repr__(self):
         return f'{self.username}'
+
+
+class WorkflowState(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    area_id = db.Column(db.Integer, db.ForeignKey('area.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    next_id = db.Column(db.Integer, db.ForeignKey('workflow_state.id'))
+    travels = db.relationship('Travel', backref='workflowstate')
+    countries = db.relationship('Country', backref='workflowstate')
+    requirements = db.relationship('TypeDocument',
+                                   secondary=workflowstate_type_document,
+                                   backref='workflowstates')
+
+    @property
+    def next(self):
+        return WorkflowState.query.get(self.next_id)
+
+    @next.setter
+    def next(self, next):
+        self.next_id = next.id
+    
+    @property
+    def previous(self):
+        return WorkflowState.query.filter(WorkflowState.next_id==self.id).all()
+
+    def __repr__(self):
+        return f'{self.name}'
