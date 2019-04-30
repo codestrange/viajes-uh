f5be7adce3c9c6da0ecee422e04fc721a1edf426
stdevCbermudez <cbermu.97@gmail.com>
2019-04-30 13:19:07 -0400

    Mostrar viajes en la vista de aprovación

--- app/controllers/approvement/controllers.py
+++ app/controllers/approvement/controllers.py
@@ -1,8 +1,23 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, login_required
 from . import approvement
 from ...models import Travel
 
 @approvement.route('/travels', methods=['GET'])
+@login_required
 def approve_travels():
-    return render_template('approvement/approve_travels.html')
+    travels = [
+        {
+            'name': 'Mi viaje 1',
+            'country': 'Cuba'
+        },
+        {
+            'name': 'Mi viaje 2',
+            'country': 'Canada'
+        },
+        {
+            'name': 'Mi viaje 2',
+            'country': 'Mexico'
+        }
+    ]
+    return render_template('approvement/approve_travels.html', travels=travels)
