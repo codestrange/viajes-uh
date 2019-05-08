670b0ffb457a27c1816ec0df4704cb5cf295c7d1
leynier <leynier41@gmail.com>
2019-05-07 23:28:24 -0400

    Arreglar error en la aprobación de los viajes

--- app/controllers/approve/controllers.py
+++ app/controllers/approve/controllers.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request, abort
 from flask_login import login_user, current_user, login_required
 from . import approve
-from ...models import db, Travel, User, Country, Document, WorkflowState, Role
+from ...models import db, Travel, User, Country, Document, WorkflowState, Role, TypeDocument
 from ...utils import check_conditions, user_can_decide
 
 
@@ -29,37 +29,24 @@ def reject_travel(id):
 @approve.route('/travel/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_travel_state(id):
-    if not user_can_decide(current_user, Travel.query.get(id)):
+    travel = Travel.query.get_or_404(id)
+    if not user_can_decide(current_user, travel):
         return abort(403)
-    travel = Travel.query.get(id)
-    creator = User.query.get(travel.user_id)
-    to_country = Country.query.get(travel.country_id)
-    state = WorkflowState.query.get(travel.workflow_state_id)
-    role = Role.query.get(state.role_id)
-    documents = travel.documents
-    requirements_ids = [ requirement.id for requirement in state.requirements ]
-    requirements = [ requirement for requirement in state.requirements ]
-    to_confirm_documents = [ document for document in documents if document.type_id in requirements_ids ]
+    documents = [
+        document
+        for document in travel.documents
+        if travel.workflow_state.requirements.filter_by(id=document.type_document.id).first()
+    ]
     if request.method == 'POST':
-        confirmed_docs = [ Document.query.get(int(id)) for id in request.form.getlist('confirmed_docs') ]
-        confirmed_ids = { doc.id for doc in confirmed_docs }
+        confirmed = {int(id) for id in request.form.getlist('confirmed_docs')}
         if request.form.get('accept_travel'):
             travel.confirmed_in_state = True
             db.session.add(travel)
-        for doc in confirmed_docs:
-            doc.confirmed = True
-            db.session.add(doc)
-        for doc in [ document for document in documents if document.id not in confirmed_ids ]:
-            doc.confirmed = False
-            db.session.add(doc)
+        for document in documents:
+            document.confirmed = document.id in confirmed
+            db.session.add(document)
         db.session.commit()
         if check_conditions(travel):
             return redirect(url_for('approve.approve_travels'))
-    return render_template('approve/edit_travel.html', \
-    travel=travel, \
-    creator=creator, \
-    to_country=to_country, \
-    documents=to_confirm_documents, \
-    requirements=requirements, \
-    role=role )
+    return render_template('approve/edit_travel.html', travel=travel, documents=documents)
  
