77df6b9aaa6dcd5f3ac1ea7ea43d2807acbd9044
leynier <leynier41@gmail.com>
2019-05-07 22:53:38 -0400

    Implementar pantalla de inicio y seguridad al panel de admin

--- app/admin.py
+++ app/admin.py
@@ -1,15 +1,35 @@
-from flask_admin import Admin
+from flask import abort, redirect, request, url_for
+from flask_admin import Admin, AdminIndexView as DefaultAdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView as _ModelView
+from flask_login import current_user
 from wtforms import PasswordField
 from wtforms.validators import DataRequired
 
-admin = Admin(name='Viajes UH', template_mode='bootstrap3')
+
+class AdminIndexView(DefaultAdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if not current_user.is_authenticated:
+            return redirect(url_for('auth.login'))
+        if not current_user.is_administrator:
+            abort(403)
+        return self.render('admin.html')
 
 
 class ModelView(_ModelView):
     can_delete = True
     can_view_details = True
     column_display_pk = True
+
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_administrator
+
+    def inaccessible_callback(self, name, **kwargs):
+        if current_user.is_authenticated:
+            abort(403)
+        else:
+            return redirect(url_for('auth.login', next=request.url))
 
 
 class UserModelView(ModelView):
@@ -20,3 +40,6 @@ class UserModelView(ModelView):
 
     def on_model_change(self, form, model, is_created):
         model.password = form.password.data
+
+
+admin = Admin(name='Viajes UH', template_mode='bootstrap3', index_view=AdminIndexView())
